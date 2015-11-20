e5edd2481c2328735d961dd43ad93ee1a07f7b74
keskitj3 <juhani.keskitalo@aalto.fi>
2015-11-20 08:26:18 +0200

    Update pgrouting.js

--- pgrouting.js
+++ pgrouting.js
@@ -112,18 +112,27 @@ map.addLayer(layerVector);
 */
 
 	//geolocation
-
-	var geolocation = new ol.Geolocation({
-  projection: "ESPG:4326"
+var geolocation = new ol.Geolocation({
+  projection: view.getProjection()
 });
 
 function el(id) {
   return document.getElementById(id);
 }
 
-el('locate').addEventListener('change', function() {
+el('track').addEventListener('change', function() {
   geolocation.setTracking(this.checked);
 });
+
+// update the HTML page when the position changes.
+geolocation.on('change', function() {
+  el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
+  el('altitude').innerText = geolocation.getAltitude() + ' [m]';
+  el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
+  el('heading').innerText = geolocation.getHeading() + ' [rad]';
+  el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
+});
+
 // handle geolocation error.
 geolocation.on('error', function(error) {
   var info = document.getElementById('info');
