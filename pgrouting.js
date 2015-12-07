a72cd2f08dde6202f5dce6d026997cf34a5b4dfb
keskitj3 <juhani.keskitalo@aalto.fi>
2015-12-07 20:58:53 +0200

    Update pgrouting.js

--- pgrouting.js
+++ pgrouting.js
@@ -125,7 +125,7 @@ var el = document.getElementById('name');
     async: false,
     success: function(json1) {
         $.each(json1, function (key, data) {
-            el.innerHTML += key+"a1"+data+"a2"+'<br>'+'<br>';
+            el.innerHTML += key+"a1"+data+"a2"+json1 +'<br>'+'<br>';
             if (key == 'features') {
 
                 $.each(data, function (k, v) {
