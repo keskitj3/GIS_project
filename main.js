fe21744f03fd5894ed9ba4bb7ad15132350b90a0
keskitj3 <juhani.keskitalo@aalto.fi>
2015-12-08 09:36:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -143,6 +143,56 @@ function init() {
 
 	map.addLayer(wfs_layer);
 
+//clusterointikokeilu
+
+var source = new ol.source.Vector({
+    url: 'http://130.233.249.20:8080/geoserver/wfs?service=WFS&' +
+	  	'version=1.1.0&request=GetFeature&typename=WMS:WFS_pisteet&' +
+		'outputFormat=application/json&srsname=EPSG:3857&' +
+		'maxFeatures=5000&' +
+		//'bbox=' + extent.join(',') + 
+		',EPSG:3857';
+    format: new ol.format.GeoJSON()
+});
+
+var clusterSource = new ol.source.Cluster({
+  distance: 40,
+  source: source
+});
+
+var styleCache = {};
+var clusters = new ol.layer.Vector({
+  source: clusterSource,
+  style: function(feature, resolution) {
+    var size = feature.get('features').length;
+    var style = styleCache[size];
+    if (!style) {
+      style = [new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 10,
+          stroke: new ol.style.Stroke({
+            color: '#fff'
+          }),
+          fill: new ol.style.Fill({
+            color: '#3399CC'
+          })
+        }),
+        text: new ol.style.Text({
+          text: size.toString(),
+          fill: new ol.style.Fill({
+            color: '#fff'
+          })
+        })
+      })];
+      styleCache[size] = style;
+    }
+    return style;
+  }
+});
+
+
+
+
 
 
 	//Layerin nakyvyys napin takana
